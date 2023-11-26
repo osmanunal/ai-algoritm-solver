ce7039be73cf294fe2c9cb76c3be76c5fa0c8dea
Osman Ünal <osmannunal11@gmail.com>
2023-11-26 20:12:03 +0300

    main

--- main.py
+++ main.py
@@ -8,29 +8,33 @@ if __name__ == "__main__":
         data = yaml.safe_load(file)
 
     while True:
-        print('Please select the question number you want to run:')
-        for key in data:
-            print(key)
+        print('\n\nSoru listesi:')
+        for idx,key in enumerate(data):
+            print(f'{idx+1}: {key}')
         print('q: quit')
         question = input('Please enter the question number: ')
         if question == 'q':
             break
-        elif question not in data:
-            print('Invalid question number')
-        else:
-            break
-    if 'knn' in data:
-        k = data['knn']['k']
-        quiz = data['knn']['quiz']
-        knn(data, k, quiz)
 
-    if 'k-means' in data:
-        values = data['k-means']['values']
-        initial_centroids = data['k-means']['cendroids']
-        k = len(initial_centroids)
-        k_means(values, initial_centroids, k)
-
-    if 'confusion_matrix' in data:
-        y_true = data['confusion_matrix']['actual']
-        y_pred = data['confusion_matrix']['predicted']
-        confusion_matrix_cal(y_true, y_pred)
+        if question == '1':
+            if 'knn' in data:
+                k = data['knn']['k']
+                quiz = data['knn']['quiz']
+                knn(data, k, quiz)
+            else :
+                print('Knn sorusu bulunamadı.')
+        elif question == '2':
+            if 'k-means' in data:
+                values = data['k-means']['values']
+                initial_centroids = data['k-means']['cendroids']
+                k = len(initial_centroids)
+                k_means(values, initial_centroids, k)
+            else :
+                print('K-means sorusu bulunamadı.')
+        elif question == '3':
+            if 'confusion_matrix' in data:
+                y_true = data['confusion_matrix']['actual']
+                y_pred = data['confusion_matrix']['predicted']
+                confusion_matrix_cal(y_true, y_pred)
+            else :
+                print('Confusion matrix sorusu bulunamadı.')
